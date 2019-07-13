49026bda74802353167c8982a48872b586822fbf
Naresh <test@example.com>
2019-07-13 18:45:05 +0530

    store and fetch

--- example1/views.py
+++ example1/views.py
@@ -1,8 +1,24 @@
 from django.shortcuts import render
 
+from website.models import UserProfile
+
 
 def hello_view(request):
     if request.method == "POST":
-        pass
+        name = request.POST.get("name")
+        email = request.POST.get("email")
+        mobile_no = request.POST.get("pn_no")
+        place = request.POST.get("place")
 
-    return render(request, "home.html")
+        data_dict = dict()
+        data_dict["name"] = name
+        data_dict["place"] = place
+        data_dict["mobile_no"] = mobile_no
+        data_dict["email"] = email
+
+        up = UserProfile(**data_dict)
+        up.save()
+
+        ups = UserProfile.objects.all()[:10]
+
+    return render(request, "home.html", locals())
