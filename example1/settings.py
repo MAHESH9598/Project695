e214c77c8382d896164cd390d4007b9775da82ad
Naresh <test@example.com>
2019-07-13 16:24:36 +0530

    host

--- example1/settings.py
+++ example1/settings.py
@@ -25,7 +25,9 @@ SECRET_KEY = 'up_e+uuq-0g23t!&x27x^05jw5#jostefr2%4g0ei#cetgt(pt'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    "*.ap-south-1.elasticbeanstalk.com",
+]
 
 
 # Application definition
@@ -73,12 +75,12 @@ WSGI_APPLICATION = 'example1.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
 
 
 # Password validation
